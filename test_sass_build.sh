02df6a0c78aff9f0b3e370410fc4b57821ab1b4d
Fred Marecesche <fred@froddd.co.uk>
2018-01-05 07:46:20 +0000

    Test on all compilers

--- test_sass_build.sh
+++ test_sass_build.sh
@@ -5,23 +5,22 @@ set -e
 
 echo "Checking all core and components files can be built individually..."
 
+COMPILERS="sass
+node-sass"
+
 FILES="./styles/_*.scss
 ./styles/components/_*.scss"
 
-for f in $FILES
+for c in $COMPILERS
 do
-  echo -n "$f "
-  node-sass $f > /dev/null
-  if [ $? -eq 0 ]; then
-    echo "OK"
-  fi
+  echo -ne "\nUsing $c compiler\n---\n"
+
+  for f in $FILES
+  do
+    echo -n "$f "
+    $c $f > /dev/null
+    if [ $? -eq 0 ]; then
+      echo "OK"
+    fi
+  done
 done
-
-echo ""
-
-echo -n "Checking full build using Ruby Sass compiler... "
-
-sass styles/_coop-toolkit.scss > /dev/null
-if [ $? -eq 0 ]; then
-  echo "OK"
-fi
